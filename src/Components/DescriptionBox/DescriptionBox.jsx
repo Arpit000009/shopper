50220e7499492d0293065099d5fc63cde0c1225e
Arpit000009 <arpitpandey000009@gmail.com>
2024-07-15 14:04:18 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="discriptiobox-navigator">
+        <div className="discriptionbox-nav-box">Description</div>
+        <div className="discriptionbox-nav-box fade">reviews(122)</div>
+      </div>
+        <div className="discriptionbox-description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed doloribus harum a adipisci vitae eius neque quam, voluptatum necessitatibus, quae sunt perferendis delectus enim id obcaecati accusantium nesciunt nulla quibusdam, quisquam ad ab perspiciatis consequuntur quidem! Ipsa cupiditate iste quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quibusdam facere assumenda at, distinctio unde incidunt tenetur neque sed commodi.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eaque, cupiditate doloremque iusto ex molestias itaque nostrum rerum saepe reiciendis.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
